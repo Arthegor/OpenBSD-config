9a847555bc94d31780b5ec71f85948af42a08f8b
Arthegor <stockee-cachemire-01@icloud.com>
2024-10-20 17:37:47 +0200

    refresh dwm

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,38 +1,25 @@
 /* See LICENSE file for copyright and license details. */
-/* #include <X11/XF86keysym.h> */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = "3270 Nerd Font Mono:style:regular:size=16";
-static const char dmenufont[]       = "3270 Nerd Font Mono:style:regular:size=12";
-
-
-// #include "tokyonight.h" /* theme color*/
-
-// static const char *colors[][3]      = {
-//     /*                     fg       bg      border */
-//     [SchemeNorm]       = { gray3,   black,  gray2 },
-//     [SchemeSel]        = { gray4,   blue,   blue  },
-//     [SchemeTitle]      = { white,   black,  black }, // active window title
-//     [TabSel]           = { blue,    gray2,  black },
-//     [TabNorm]          = { gray3,   black,  black },
-//     [SchemeTag]        = { gray3,   black,  black },
-//     [SchemeTag1]       = { blue,    black,  black },
-//     [SchemeTag2]       = { red,     black,  black },
-//     [SchemeTag3]       = { orange,  black,  black },
-//     [SchemeTag4]       = { green,   black,  black },
-//     [SchemeTag5]       = { pink,    black,  black },
-//     [SchemeLayout]     = { green,   black,  black },
-//     [SchemeBtnPrev]    = { green,   black,  black },
-//     [SchemeBtnNext]    = { yellow,  black,  black },
-//     [SchemeBtnClose]   = { red,     black,  black },
-// };
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
 
 /* tagging */
-static const char *tags[] = { "", "󰊯", "", "&" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -40,15 +27,14 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "st",      NULL,       NULL,        0,           1,           -1 },
-	{ "Browser", NULL,       NULL,        1 << 8,      0,           -1 },
-	{ "File",    NULL,       NULL,        0,           1,           -1 },
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
@@ -72,20 +58,12 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-
+static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
-
-	// brightness and audio 
-    // {0,             XF86XK_AudioLowerVolume,    spawn, {.v = downvol}},
-	// {0,             XF86XK_AudioMute, spawn,    {.v = mutevol }},
-	// {0,             XF86XK_AudioRaiseVolume,    spawn, {.v = upvol}},
-	// {0,				XF86XK_MonBrightnessUp,     spawn,	{.v = light_up}},
-	// {0,				XF86XK_MonBrightnessDown,   spawn,	{.v = light_down}},
-
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          SHCMD("st")},
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -93,7 +71,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
@@ -116,7 +94,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY,                       XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
 /* button definitions */
@@ -126,7 +104,7 @@ static const Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          SHCMD("st")},
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
@@ -135,3 +113,4 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
+
--- dwm/config.mk
+++ dwm/config.mk
@@ -5,7 +5,7 @@ VERSION = 6.5
 
 # paths
 PREFIX = /usr/local
-MANPREFIX = ${PREFIX}/man
+MANPREFIX = ${PREFIX}/share/man
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
@@ -16,8 +16,10 @@ XINERAMAFLAGS = -DXINERAMA
 
 # freetype
 FREETYPELIBS = -lfontconfig -lXft
-FREETYPEINC = ${X11INC}/freetype2
-
+FREETYPEINC = /usr/include/freetype2
+# OpenBSD (uncomment)
+#FREETYPEINC = ${X11INC}/freetype2
+#MANPREFIX = ${PREFIX}/man
 
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC}
